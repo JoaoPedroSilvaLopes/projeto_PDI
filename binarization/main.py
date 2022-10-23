dbd6ee85b688c3c2fad720e97b39c91e5b864674
JoaoPedroSilvaLopes <joaopedroS.L@hotmail.com>
2022-10-23 15:29:19 -0300

    feat: organizar pastas de imagens bases e imagens resultantes

--- binarization/main.py
+++ binarization/main.py
@@ -1,10 +1,10 @@
 import cv2;
 
-img = cv2.imread('images/JPCLN001.png')
+img = cv2.imread('images-base/JPCLN001.png')
 
 th, img_th = cv2.threshold(img, 128, 192, cv2.THRESH_BINARY)
 
 print(th)
 # 128.0
 
-cv2.imwrite('images/JPCLN001.png', img_th)
+cv2.imwrite('images-result/JPCLN001.png', img_th)
