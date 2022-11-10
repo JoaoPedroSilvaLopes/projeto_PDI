f4e75f123989a992804d87954cba044cb6e72979
joao pedro <finkl1@hotmail.com>
2022-11-10 19:22:23 -0300

    feat: adição de tratamento contra ruído, remoção de áreas brancas e remoção de resquícios do plano de fundo

--- binarization/areas-selected.py
+++ binarization/areas-selected.py
@@ -1,57 +1,35 @@
 from tkinter import Image
 from skimage import *
+import copy
 import cv2;
+
+# REALIZAR A LIMIARIZAÇÃO
 
 cv2.namedWindow("output", cv2.WINDOW_NORMAL) 
 
-image = cv2.imread('images-base/JPCNN001.png')
+image = cv2.imread('projeto_PDI/images-base/JPCNN001.png')
 
-# # tamanho do lado da região é de 146px
-# # x: 5 - 6 | y: 3 - 4
-# #cv2.rectangle(img=image, pt1=(700, 604), pt2=(846, 750), color=(0,0,0), thickness=2)
-# subimg1 = image[700:846, 604:750]
-
-# # x: 2 - 3 | y: 4 - 5
-# #cv2.rectangle(img=image, pt1=(262, 750), pt2=(408, 896), color=(0,0,0), thickness=2)
-# subimg2 = image[262:408, 750:896]
-
-# # x: 4 - 5 | y: 4 - 5
-# #cv2.rectangle(img=image, pt1=(554, 750), pt2=(700, 896), color=(0,0,0), thickness=2)
-# subimg3 = image[554:700, 750:896]
-
-# # x: 3 - 4 | y: 5 - 6
-# #cv2.rectangle(img=image, pt1=(408, 896), pt2=(554, 1042), color=(0,0,0), thickness=2)
-# subimg4 = image[408:554, 896:1042]
-
-# # x: 4 - 5 | y: 6 - 7
-# #cv2.rectangle(img=image, pt1=(554, 1042), pt2=(700, 1188), color=(0,0,0), thickness=2)
-# subimg5 = image[554:700, 1042:1188]
-
-# # x: 3 - 4 | y: 7 - 8
-# #cv2.rectangle(img=image, pt1=(408, 1188), pt2=(554, 1334), color=(0,0,0), thickness=2)
-# subimg6 = image[408:554, 1188:1334]
-
-cv2.rectangle(img=image, pt1=(680, 620), pt2=(820, 760), color=(0,0,0), thickness=2)
+# cv2.rectangle(img=image, pt1=(680, 620), pt2=(820, 760), color=(0,0,0), thickness=2)
 subimg1 = image[680:820, 620:760]
 
 # x: 2 - 3 | y: 4 - 5
-cv2.rectangle(img=image, pt1=(260, 760), pt2=(400, 900), color=(0,0,0), thickness=2)
+# cv2.rectangle(img=image, pt1=(260, 760), pt2=(400, 900), color=(0,0,0), thickness=2)
 subimg2 = image[260:400, 760:900]
 
 # x: 4 - 5 | y: 4 - 5
-cv2.rectangle(img=image, pt1=(540, 760), pt2=(680, 900), color=(0,0,0), thickness=2)
+# cv2.rectangle(img=image, pt1=(540, 760), pt2=(680, 900), color=(0,0,0), thickness=2)
 subimg3 = image[540:680, 760:900]
 
 # x: 3 - 4 | y: 5 - 6
-cv2.rectangle(img=image, pt1=(400, 900), pt2=(540, 1040), color=(0,0,0), thickness=2)
+# cv2.rectangle(img=image, pt1=(400, 900), pt2=(540, 1040), color=(0,0,0), thickness=2)
 subimg4 = image[400:540, 900:1040]
 
 # x: 4 - 5 | y: 6 - 7
-cv2.rectangle(img=image, pt1=(540, 1040), pt2=(680, 1180), color=(0,0,0), thickness=2)
+# cv2.rectangle(img=image, pt1=(540, 1040), pt2=(680, 1180), color=(0,0,0), thickness=2)
 subimg5 = image[540:680, 1040:1180]
 
 # x: 3 - 4 | y: 7 - 8
-cv2.rectangle(img=image, pt1=(400, 1180), pt2=(540, 1320), color=(0,0,0), thickness=2)
+# cv2.rectangle(img=image, pt1=(400, 1180), pt2=(540, 1320), color=(0,0,0), thickness=2)
 subimg6 = image[400:540, 1180:1320]
 
 grayScaleAreas = sorted([
@@ -63,18 +41,61 @@ grayScaleAreas = sorted([
   round(subimg6.mean())
 ])
 
-print(grayScaleAreas)
-
 # deletar primeiro e ultimo elemento do array
 del grayScaleAreas[5]
 del grayScaleAreas[0]
 
-# fazer a média dos 4 valores restantes e arrendonda-lo
+# ENCONTRANDO O LIMIAR DA BINARIZAÇÃO
 limiar_m = round(sum(grayScaleAreas) / 4)
 
-print(limiar_m)
+# REALIZAÇÃO DA BINARIZAÇÃO
+th, imagem_binarizada_inicial = cv2.threshold(image, limiar_m, 255, cv2.THRESH_BINARY_INV)
 
-th, imagem_binarizada = cv2.threshold(image, limiar_m, 255, cv2.THRESH_BINARY_INV)
+# ETAPA TORNAR 50px EM PIXEIS PRETOS,
+imagem_binarizada_preenchida = copy.deepcopy(imagem_binarizada_inicial)
 
-cv2.imshow("output", image)
+# PINTAR PARTE DE CIMA DA IMAGEM
+imagem_binarizada_preenchida[0:50, 0:2048] = (0, 0, 0)
+# PINTAR LADO ESQUERDO DA IMAGEM
+imagem_binarizada_preenchida[0:2048, 0:50] = (0, 0, 0)
+# PINTAR PARTE DE BAIXO DA IMAAGEM
+imagem_binarizada_preenchida[0:2048, 1998:2048] = (0, 0, 0)
+# PINTAR LADO DIREITO DA IMAGEM
+imagem_binarizada_preenchida[1998:2048, 0:2048] = (0, 0, 0)
+
+# LAÇOS FORs PARA PREENCHIMENTO DA IMAGEM
+for i in range(50, 1998):
+  for j in range(50, 1998):
+    if imagem_binarizada_preenchida[j, i, 0] == 0:
+      break
+    else:
+      imagem_binarizada_preenchida[j, i]=[0,0,0]
+  
+  for j in reversed(range(50, 1998)):
+    if imagem_binarizada_preenchida[j, i, 0] == 0:
+      break
+    else:
+      imagem_binarizada_preenchida[j, i]=[0,0,0]
+
+# REMOÇÃO DE RUÍDO APÓS O PREENCHIMENTO
+imagem_ruido = cv2.subtract(imagem_binarizada_inicial, imagem_binarizada_preenchida)
+
+disco_60 = cv2.getStructuringElement
+
+# REALIZAÇÃO DA DILATAÇÃO COM 3 ELEMENTOS ESTRUTURANTES
+imagem_ruido_dilatado = copy.deepcopy(imagem_ruido)
+
+imagem_ruido_dilatado = cv2.dilate(imagem_ruido_dilatado, cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (120, 120)))
+imagem_ruido_dilatado = cv2.dilate(imagem_ruido_dilatado, cv2.getStructuringElement(cv2.MORPH_RECT, (30, 10)))
+imagem_ruido_dilatado = cv2.dilate(imagem_ruido_dilatado, cv2.getStructuringElement(cv2.MORPH_RECT, (10, 30)))
+
+# OBTENÇÃO DA IMAGEM RESULTADO PELA SUBTRAÇÃO DA IMAGEM BINARIZADA PELO RUIDO DILATADO
+imagem_subtraida = cv2.subtract(imagem_binarizada_inicial, imagem_ruido_dilatado)
+
+# REALIZAÇÃO DE OPERAÇÕES MORFOLÓGICAS DE FECHAMENTO
+#imagem_subtraida = cv2.morphologyEx(imagem_subtraida, cv2.MORPH_CLOSE, cv2.getStructuringElement(cv2.MORPH_RECT, (35, 35)))
+#imagem_subtraida = cv2.dilate(imagem_subtraida, cv2.getStructuringElement(cv2.MORPH_RECT, (35, 35)))
+#imagem_subtraida = cv2.morphologyEx(imagem_subtraida, cv2.MORPH_CLOSE, cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (70, 70)))
+
+cv2.imshow("output", imagem_subtraida)
 cv2.waitKey(0)
